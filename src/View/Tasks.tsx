be1c59652a49507fbc618d9e121a48941eac4598
gogy9000 <goropashnyj9000@gmail.com>
2022-09-13 18:07:00 +0500

    refactoring

--- src/View/Tasks.tsx
+++ src/View/Tasks.tsx
@@ -1,6 +1,6 @@
 import React, {memo} from "react";
 import {Task} from "./Task";
-import {FlatList, ListRenderItem, Text, View} from "react-native";
+import {FlatList, ListRenderItem,View} from "react-native";
 import {TaskItem, TodoListItem} from "../DAL/types/types";
 
 type TasksProps = {
@@ -11,10 +11,7 @@ export const Tasks: React.FC<TasksProps> = memo(({tasks, todo}) => {
 
     const render: ListRenderItem<TaskItem> = ({item}) => {
         return (
-            <>
-                <Text>{item.title}</Text> :
                 <Task todo={todo} key={item.id} task={item}/>
-            </>
         )
     }
 
