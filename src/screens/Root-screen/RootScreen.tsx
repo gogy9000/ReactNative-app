549e7b100a27e382939c224be39c719000b9fee2
gogy9000 <goropashnyj9000@gmail.com>
2022-10-11 22:48:40 +0500

    migration on main server

--- src/screens/Root-screen/RootScreen.tsx
+++ src/screens/Root-screen/RootScreen.tsx
@@ -10,7 +10,7 @@ export const RootScreen = () => {
     return (
         <Tab.Navigator
             tabBar={() => <></>}
-            sceneContainerStyle={{backgroundColor: "rgba(5,5,5,0)"}}
+            sceneContainerStyle={{backgroundColor: "rgba(5,5,5,1)"}}
             screenOptions={{headerShown: false,}}>
             <Tab.Screen name={"Login"} component={Login}/>
             <Tab.Screen name={"TodoScreen"} component={TodoScreen}/>
